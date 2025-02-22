7bddfc1afb068cdb90029949dcbd87a6668eb64a
aittaleb <aittaleb@insa-toulouse.fr>
2025-02-22 21:45:25 +0100

    add secrets file example

---
+++ PrepareAccounts/secrets/secretvars.example.tf
@@ -0,0 +1,10 @@
+# secretvars.tf
+
+
+project_id = "<Provide An ID>"
+user_emails = [
+    "person1@example.com",
+    "person2@example.com",
+    "person3@example.com",
+    "person4@example.com"
+  ] 
