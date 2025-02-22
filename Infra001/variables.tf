4fd4e9396de30dc4ca4dc6f3554d09edb3ec6bb4
aittaleb <aittaleb@insa-toulouse.fr>
2025-02-22 23:05:11 +0100

    add the variables

---
+++ Infra001/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "Region where the ressource are deployed"
+  type        = string
+}
+
+variable "zone" {
+  description = "Zone where the ressource are deployed"
+  type        = string
+}
+
+variable "project_id" {
+  description = "ID of the project"
+  type        = string
+}
